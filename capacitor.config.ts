83d1e3cafb78e9a8d305b1fb164e5b5584f443e9
xsantanax <rafaelsantana111@gmail.com>
2021-08-01 08:37:43 -0300

    npm cap init

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'capacitor-test',
+  webDir: 'public',
+  bundledWebRuntime: false
+}
+
+export default config
